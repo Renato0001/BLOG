36de7565255b1c341202547d9ff6da3a4d60b885
Renato <boniex01@gmail.com>
2023-02-18 15:29:29 -0300

    novo

---
+++ header.php
@@ -0,0 +1,22 @@
+<?php
+  include_once("helpers/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Codar</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= $BASE_URL?>/css/styles.css">
+    <!-- gogle fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;1,300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        Template cabeçalho
+    </header>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    include_once("templates/header.php")
+?>
+    <h1>Olá Mundo</h1>
+    
+<?php
+    include_once("templates/footer.php")
+?>
